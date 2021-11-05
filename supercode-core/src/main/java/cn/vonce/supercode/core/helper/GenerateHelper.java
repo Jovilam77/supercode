ded7d9f3fc314de53f93ac2ad50119c4dad806bf
Jovi <imjovi@qq.com>
2021-11-05 18:04:50 +0800

    1：完善没有配置生成目标路径时生成到默认路径； 2：优化生成助手类；

--- supercode-core/src/main/java/cn/vonce/supercode/core/helper/GenerateHelper.java
+++ supercode-core/src/main/java/cn/vonce/supercode/core/helper/GenerateHelper.java
@@ -11,11 +11,13 @@ import cn.vonce.supercode.core.model.FiledInfo;
 import cn.vonce.supercode.core.model.ClassInfo;
 import cn.vonce.supercode.core.type.JdbcDaoType;
 import cn.vonce.supercode.core.util.FreemarkerUtil;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import freemarker.template.Configuration;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * 生成助手
@@ -27,58 +29,38 @@ import java.util.*;
  */
 public class GenerateHelper {
 
-    public static void start(GenerateConfig config, TableService tableService) throws IOException {
-        List<ClassInfo> classInfoList = getGenerateObjectList(config, tableService);
-        FreemarkerUtil freemarkerUtil = getFreemarkerUtil(config);
-        File targerDir = null;
-        File modelDir;
-        File mapperDir;
-        File serviceDir;
-        File serviceImplDir;
-        File controllerDir;
-        File sqlDocDir;
-        String packPath = config.getBasePackage().replace(".", File.separator);
-        String dateString = DateUtil.dateToString(new Date(), "yyyyMMddHHmmss");
-        if (StringUtil.isNotEmpty(config.getTargetPath())) {
-            targerDir = new File(config.getTargetPath() + File.separator + dateString);
-        } else {
-            //默认生成地址
+    public static void build(GenerateConfig config, TableService tableService) throws IOException {
+        List<TableInfo> tableInfoList = getTableInfoList(config, tableService);
+        Map<String, String> filePaths = getFilePaths(config);
+        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
+        ExecutorService pool = new ThreadPoolExecutor(5, 10,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(1024), threadFactory, new ThreadPoolExecutor.AbortPolicy());
+        int num = 10;
+        int size = tableInfoList.size() % num == 0 ? tableInfoList.size() / num : tableInfoList.size() / num + 1;
+        for (int i = 0; i < size; i++) {
+            int finalI = i;
+            pool.execute(() -> {
+                List<ClassInfo> classInfoList = getGenerateObjectList(config, tableInfoList.subList(num * finalI, finalI == size - 1 ? tableInfoList.size() : num * finalI + num), tableService);
+                try {
+                    make(config, filePaths, classInfoList);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
-        if (!targerDir.exists()) {
-            targerDir.mkdirs();
-        }
-        modelDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "model");
-        mapperDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + (config.getJdbcDaoType() == JdbcDaoType.MyBatis ? "mapper" : "jdbc"));
-        serviceDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "service");
-        serviceImplDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "service" + File.separator + "impl");
-        controllerDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "controller");
-        sqlDocDir = new File(targerDir.getAbsolutePath() + File.separator + "dbDoc");
-        modelDir.mkdirs();
-        mapperDir.mkdirs();
-        serviceDir.mkdirs();
-        serviceImplDir.mkdirs();
-        controllerDir.mkdirs();
-        sqlDocDir.mkdirs();
-//        ThreadFactory threadFactory = new ThreadFactoryBuilder().setNameFormat("demo-pool-%d").build();
-//        ExecutorService pool = new ThreadPoolExecutor(5, 20,
-//                0L, TimeUnit.MILLISECONDS,
-//                new LinkedBlockingQueue<>(1024), threadFactory, new ThreadPoolExecutor.AbortPolicy());
-        for (ClassInfo classInfo : classInfoList) {
-            freemarkerUtil.fprint(classInfo, "model.ftl", modelDir.getAbsolutePath() + File.separator + classInfo.getClassName() + ".java");
-            freemarkerUtil.fprint(classInfo, "mapper.ftl", mapperDir.getAbsolutePath() + File.separator + classInfo.getClassName() + (config.getJdbcDaoType() == JdbcDaoType.MyBatis ? "Mapper.java" : "Jdbc.java"));
-            freemarkerUtil.fprint(classInfo, "service.ftl", serviceDir.getAbsolutePath() + File.separator + classInfo.getClassName() + "Service.java");
-            freemarkerUtil.fprint(classInfo, "service_impl.ftl", serviceImplDir.getAbsolutePath() + File.separator + classInfo.getClassName() + "ServiceImpl.java");
-            freemarkerUtil.fprint(classInfo, "controller.ftl", controllerDir.getAbsolutePath() + File.separator + classInfo.getClassName() + "Controller.java");
-            freemarkerUtil.fprint(classInfo, config.getJdbcDocType().getTemplateName(), sqlDocDir.getAbsolutePath() + File.separator + classInfo.getTableInfo().getName() + config.getJdbcDocType().getSuffix());
-        }
-//        pool.shutdown();
+        pool.shutdown();
     }
 
-    public static List<ClassInfo> getGenerateObjectList(GenerateConfig config, TableService tableService) {
+    public static List<TableInfo> getTableInfoList(GenerateConfig config, TableService tableService) {
         List<TableInfo> tableInfoList = tableService.getTableList(null);
         if (tableInfoList == null || tableInfoList.isEmpty()) {
             return null;
         }
+        return tableInfoList;
+    }
+
+    public static List<ClassInfo> getGenerateObjectList(GenerateConfig config, List<TableInfo> tableInfoList, TableService tableService) {
         Date date = new Date();
         List<ClassInfo> classInfoList = new ArrayList<>();
         for (TableInfo tableInfo : tableInfoList) {
@@ -126,6 +108,59 @@ public class GenerateHelper {
         return classInfoList;
     }
 
+    public static void make(GenerateConfig config, Map<String, String> filePaths, List<ClassInfo> classInfoList) throws IOException {
+        FreemarkerUtil freemarkerUtil = getFreemarkerUtil(config);
+        for (ClassInfo classInfo : classInfoList) {
+            freemarkerUtil.fprint(classInfo, "model.ftl", filePaths.get("model") + File.separator + classInfo.getClassName() + ".java");
+            freemarkerUtil.fprint(classInfo, "mapper.ftl", filePaths.get("mapper") + File.separator + classInfo.getClassName() + (config.getJdbcDaoType() == JdbcDaoType.MyBatis ? "Mapper.java" : "Jdbc.java"));
+            freemarkerUtil.fprint(classInfo, "service.ftl", filePaths.get("service") + File.separator + classInfo.getClassName() + "Service.java");
+            freemarkerUtil.fprint(classInfo, "service_impl.ftl", filePaths.get("serviceImpl") + File.separator + classInfo.getClassName() + "ServiceImpl.java");
+            freemarkerUtil.fprint(classInfo, "controller.ftl", filePaths.get("controller") + File.separator + classInfo.getClassName() + "Controller.java");
+            freemarkerUtil.fprint(classInfo, config.getJdbcDocType().getTemplateName(), filePaths.get("sqlDocDir") + File.separator + classInfo.getTableInfo().getName() + config.getJdbcDocType().getSuffix());
+        }
+    }
+
+    public static Map<String, String> getFilePaths(GenerateConfig config) {
+        Map<String, String> filePaths = new HashMap<>();
+        File targerDir;
+        File modelDir;
+        File mapperDir;
+        File serviceDir;
+        File serviceImplDir;
+        File controllerDir;
+        File sqlDocDir;
+        String packPath = config.getBasePackage().replace(".", File.separator);
+        String dateString = DateUtil.dateToString(new Date(), "yyyyMMddHHmmss");
+        if (StringUtil.isNotEmpty(config.getTargetPath())) {
+            targerDir = new File(config.getTargetPath() + File.separator + dateString);
+        } else {
+            //默认生成地址
+            targerDir = new File(System.getProperty("user.dir") + File.separator + "generateTarget" + File.separator + dateString);
+        }
+        if (!targerDir.exists()) {
+            targerDir.mkdirs();
+        }
+        modelDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "model");
+        mapperDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + (config.getJdbcDaoType() == JdbcDaoType.MyBatis ? "mapper" : "jdbc"));
+        serviceDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "service");
+        serviceImplDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "service" + File.separator + "impl");
+        controllerDir = new File(targerDir.getAbsolutePath() + File.separator + packPath + File.separator + "controller");
+        sqlDocDir = new File(targerDir.getAbsolutePath() + File.separator + "dbDoc");
+        modelDir.mkdirs();
+        mapperDir.mkdirs();
+        serviceDir.mkdirs();
+        serviceImplDir.mkdirs();
+        controllerDir.mkdirs();
+        sqlDocDir.mkdirs();
+        filePaths.put("model", modelDir.getAbsolutePath());
+        filePaths.put("mapper", mapperDir.getAbsolutePath());
+        filePaths.put("service", serviceDir.getAbsolutePath());
+        filePaths.put("serviceImpl", serviceImplDir.getAbsolutePath());
+        filePaths.put("controller", controllerDir.getAbsolutePath());
+        filePaths.put("sqlDocDir", sqlDocDir.getAbsolutePath());
+        return filePaths;
+    }
+
     /**
      * 获取
      *
@@ -138,7 +173,7 @@ public class GenerateHelper {
             File file = new File(config.getTemplatePath());
             freemarkerUtil = FreemarkerUtil.getInstance(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS.toString(), file);
         } else {
-            freemarkerUtil = FreemarkerUtil.getInstance(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS.toString(), "/resources/");
+            freemarkerUtil = FreemarkerUtil.getInstance(Configuration.DEFAULT_INCOMPATIBLE_IMPROVEMENTS.toString(), "/template/");
         }
         return freemarkerUtil;
     }
