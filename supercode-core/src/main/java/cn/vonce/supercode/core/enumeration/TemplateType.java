a3d28b3c44b43e2694e614c1ac863b2037fc6ae3
Jovi <imjovi@qq.com>
2023-08-01 11:41:10 +0800

    优化模板与生成路径

--- supercode-core/src/main/java/cn/vonce/supercode/core/enumeration/TemplateType.java
+++ supercode-core/src/main/java/cn/vonce/supercode/core/enumeration/TemplateType.java
@@ -1,30 +1,13 @@
 package cn.vonce.supercode.core.enumeration;
 
 /**
+ * 模板类型
+ *
  * @author Jovi
  * @email imjovi@qq.com
- * @date 2023/6/16 17:10
+ * @date 2023/8/1 10:54
  */
 public enum TemplateType {
-
-    MODEL("model.ftl"),
-    MAPPER("mapper.ftl"),
-    SERVICE("service.ftl"),
-    SERVICE_IMPL("service_impl.ftl"),
-    CONTROLLER("controller.ftl"),
-    DB_HTML("db_html.ftl"),
-    DB_MARKDOWN("db_markdown.ftl"),
-    DB_WORD("db_word.ftl"),
-    SQL("sql.ftl");
-
-    TemplateType(String templateName) {
-        this.templateName = templateName;
-    }
-
-    String templateName;
-
-    public String getTemplateName() {
-        return templateName;
-    }
+    JAVA, DOC, SQL
 
 }
